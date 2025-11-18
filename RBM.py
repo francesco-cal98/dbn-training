d5e6eafbd254d9ae67e057db9023221395cc9946
pietro.renna <fra.calistroni@gmail.com>
2025-11-18 19:08:50 +0100

    fix device

--- RBM.py
+++ RBM.py
@@ -61,7 +61,6 @@ class RBM(nn.Module):
         # W: (visible_units, hidden_units)
 
         self.W = nn.Parameter(
-            torch.nn.init.xavier_normal_(torch.empty((visible_units, hidden_units), device='cpu')) * 0.01
         )
         self.v_bias = nn.Parameter(torch.zeros(visible_units, device=self.device))
         self.h_bias = nn.Parameter(torch.zeros(hidden_units, device=self.device))
